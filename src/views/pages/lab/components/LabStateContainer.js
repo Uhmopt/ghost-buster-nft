6bb12cf1aba05b743c4994b14f34f0f0df69c043
Uhmopt <qethum@gmail.com>
2022-02-26 21:28:14 +1100

    [Fix] container width

--- src/views/pages/lab/components/LabStateContainer.js
+++ src/views/pages/lab/components/LabStateContainer.js
@@ -8,7 +8,7 @@ export default function LabStateContainer({ data = [] }) {
     <div className="w-full">
       <Grid container spacing={4} justifyContent="center">
         {formatArray(data).map((item, itemIndex) => (
-          <Grid key={itemIndex} item lg={4} md={4} sm={12} xs={12}>
+          <Grid key={itemIndex} item lg={4} md={4} sm={6} xs={12}>
             <StateCard data={item ?? {}} />
           </Grid>
         ))}
